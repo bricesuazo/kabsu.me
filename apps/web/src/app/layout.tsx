b86b24a3b4f12f7074a2c4648da8ae26f24c82ed
Brice Suazo <bricesuazo@gmail.com>
2023-05-07 02:53:43 +0800

    Made the "/" a public path. Add next/font

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,10 +1,16 @@
 import "./globals.css";
+import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import { getCurrentScheme } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Analytics } from "@vercel/analytics/react";
 
 import ClientProviders from "./ClientProviders";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default async function RootLayout({
   children,
@@ -13,7 +19,10 @@ export default async function RootLayout({
 }) {
   const scheme = await getCurrentScheme();
   return (
-    <html lang="en" className={scheme === "dark" ? "dark" : ""}>
+    <html
+      lang="en"
+      className={`${scheme === "dark" ? "dark" : ""} ${inter.className}`}
+    >
       <ClerkProvider>
         <ClientProviders>
           <body>
