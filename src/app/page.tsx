f7c8395d3e218c508d6f8ca56e52978ba6676887
Rod Cotines <rodcotines@gmail.com>
2023-09-03 17:32:20 +0800

    added style on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       {userId ? (
         <>
           <PostForm />
-
+          hello world
           <Suspense fallback="Loading...">
             <Posts />
           </Suspense>
