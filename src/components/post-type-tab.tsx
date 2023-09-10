f4f64ad749694a58f6d352f91776f3dac26aadb8
Brice Suazo <bricesuazo@gmail.com>
2023-09-10 22:09:41 +0800

    Use select instead of tab

--- src/components/post-type-tab.tsx
+++ src/components/post-type-tab.tsx
@@ -1,10 +1,17 @@
 "use client";
 
 import { POST_TYPE_TABS } from "@/lib/constants";
-import { TabsList, Tabs, TabsTrigger, TabsContent } from "./ui/tabs";
 import { useRouter, useSearchParams } from "next/navigation";
-import { Component, School, School2, UserCheck2, Users2 } from "lucide-react";
 import { useEffect, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 
 export default function PostTypeTab() {
   const searchParams = useSearchParams();
@@ -18,38 +25,64 @@ export default function PostTypeTab() {
   }, [searchParams]);
 
   return (
-    <Tabs
+    <Select
+      defaultValue={tab}
       value={tab}
+      required
       onValueChange={(value) => {
         setTab(value);
         router.push(value !== "following" ? `/?tab=${value}` : "/");
       }}
     >
-      <TabsList className="w-full sm:w-auto">
-        {POST_TYPE_TABS.map((tab) => (
-          <TabsTrigger key={tab.id} value={tab.id} className="w-full">
-            <div className="hidden sm:block">{tab.name}</div>
-            <div className="sm:hidden">
-              {(() => {
-                switch (tab.id) {
-                  case "all":
-                    return <School size="1.25rem" />;
-                  case "campus":
-                    return <School2 size="1.25rem" />;
-                  case "program":
-                    return <Users2 size="1.25rem" />;
-                  case "college":
-                    return <Component size="1.25rem" />;
-                  case "following":
-                    return <UserCheck2 size="1.25rem" />;
-                  default:
-                    return null;
-                }
-              })()}
-            </div>
-          </TabsTrigger>
-        ))}
-      </TabsList>
-    </Tabs>
+      <SelectTrigger>
+        <SelectValue placeholder="Post" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Post</SelectLabel>
+          {POST_TYPE_TABS.map((tab) => (
+            <SelectItem key={tab.id} value={tab.id}>
+              {tab.name}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
+
+  // return (
+  //   <Tabs
+  //     value={tab}
+  //     onValueChange={(value) => {
+  //       setTab(value);
+  //       router.push(value !== "following" ? `/?tab=${value}` : "/");
+  //     }}
+  //   >
+  //     <TabsList className="w-full sm:w-auto">
+  //       {POST_TYPE_TABS.map((tab) => (
+  //         <TabsTrigger key={tab.id} value={tab.id} className="w-full">
+  //           <div className="hidden sm:block">{tab.name}</div>
+  //           <div className="sm:hidden">
+  //             {(() => {
+  //               switch (tab.id) {
+  //                 case "all":
+  //                   return <School size="1.25rem" />;
+  //                 case "campus":
+  //                   return <School2 size="1.25rem" />;
+  //                 case "program":
+  //                   return <Users2 size="1.25rem" />;
+  //                 case "college":
+  //                   return <Component size="1.25rem" />;
+  //                 case "following":
+  //                   return <UserCheck2 size="1.25rem" />;
+  //                 default:
+  //                   return null;
+  //               }
+  //             })()}
+  //           </div>
+  //         </TabsTrigger>
+  //       ))}
+  //     </TabsList>
+  //   </Tabs>
+  // );
 }
