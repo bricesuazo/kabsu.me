f9ec06f13d29a78a3b655eb5c61f679fef9b75fc
Brice Suazo <bricesuazo@gmail.com>
2023-09-16 19:51:37 +0800

    use edge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const font = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-// export const runtime = "edge";
+export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
