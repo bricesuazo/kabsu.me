68791a46fdd10adf805a4ed7b8800dc0c33b45e2
Brice Suazo <bricesuazo@gmail.com>
2023-09-22 15:44:52 +0800

    Add clerk auth to admin page.

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -2,11 +2,13 @@ import "@cvsu.me/tailwind-config/globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Admin - CvSU",
+  title: "Admin - CvSU.me",
   description: "Admin dashboard of CvSU.me",
 };
 
@@ -16,8 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_ADMIN_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider attribute="class">{children}</ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,3 +1,30 @@
+import { ToggleTheme } from "@/components/toggle-theme";
+import { Button } from "@/components/ui/button";
+import { auth, SignIn, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <main>test</main>;
+  const { userId } = auth();
+
+  if (!userId)
+    return (
+      <main className="grid h-full place-items-center">
+        <SignIn />
+      </main>
+    );
+
+  return (
+    <main>
+      <header className="flex items-center justify-between p-4">
+        <Button variant="link" className="h-auto p-0">
+          CvSU.me Admin Dashboard
+        </Button>
+        <div className="flex items-center gap-x-2">
+          <ToggleTheme />
+          <UserButton />
+        </div>
+      </header>
+
+      <div className="container max-w-screen-xl">Test</div>
+    </main>
+  );
 }
