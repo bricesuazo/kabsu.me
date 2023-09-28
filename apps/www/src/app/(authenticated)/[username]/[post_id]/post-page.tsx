b102546fc9f5655ef7fad10ec5aed2b19233108d
Brice Suazo <bricesuazo@gmail.com>
2023-09-29 00:07:42 +0800

    Fix build error

--- apps/www/src/app/(authenticated)/[username]/[post_id]/post-page.tsx
+++ apps/www/src/app/(authenticated)/[username]/[post_id]/post-page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Fragment, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import {
