9b38b81b9b759f1a8954d81c3d40cb926241305a
Alexis Ken Alvarez <alexisken1432@gmail.com>
2024-08-29 20:19:22 +0800

    fix: node_env

--- packages/api/src/routers/chats.ts
+++ packages/api/src/routers/chats.ts
@@ -122,7 +122,7 @@ export const chatsRouter = router({
         limiter: Ratelimit.slidingWindow(5, "10 s"),
       });
 
-      if (env.NODE_ENV === "development") {
+      if (env.NODE_ENV !== "development") {
         const { success } = await rate_limiter.limit(ctx.auth.user.id);
 
         if (!success) {
