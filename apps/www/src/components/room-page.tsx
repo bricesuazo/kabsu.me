9dfad4c95f0ee728124929fc5ae90ae220659a49
AJ Espinosa <espinosa.aaron45@gmail.com>
2024-09-09 00:03:45 +0800

    justify end send button, fix input border raduis, fix sending empty

--- apps/www/src/components/room-page.tsx
+++ apps/www/src/components/room-page.tsx
@@ -536,7 +536,7 @@ export default function RoomPageClient(
                   <FormItem className="space-y-2">
                     <FormMessage />
                     <FormControl>
-                      <div className="relative flex items-center">
+                      <div className="relative flex items-end">
                         <TextareaAutosize
                           {...field}
                           placeholder="Write a message..."
@@ -544,13 +544,13 @@ export default function RoomPageClient(
                           onKeyDown={async (e) => {
                             if (e.key === "Enter" && !e.shiftKey) {
                               e.preventDefault();
-
+                              console.log("first");
                               await handleSubmit(form.getValues());
                             }
                           }}
                           rows={1}
                           maxRows={3}
-                          className="w-full resize-none rounded-full border-input bg-background px-4 py-2 text-base text-foreground ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                          className="w-full resize-none rounded-md border-input bg-background px-4 py-2 text-base text-foreground ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                         />
                         <Button
                           type="submit"
